5cb61dd9b3e8d06b94c146ee439a593d4cff5cca
Rhett Beardemphl <rhettbeardemphl@hotmail.com>
2023-11-11 20:37:07 -0800

    Initial Commit

---
+++ Le History/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Le History
+//
+//  Created by Rhett Beardemphl :) on 11/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Le History/Le_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Le_HistoryApp.swift
+//  Le History
+//
+//  Created by Rhett Beardemphl :) on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Le_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
